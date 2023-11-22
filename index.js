092d45ff5f6baaeafe4b7cf3a6614a917488d38e
mdrubelhasan1 <mdrubelhasan360@gmail.comm>
2023-11-22 10:30:07 +0600

    submit button

--- index.js
+++ index.js
@@ -46,7 +46,8 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
+  // console.log(data);
   quizData = data;
   displayQuiz(data);
 };
@@ -59,12 +60,12 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
-    quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
+  quizContainer.innerHTML = `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -74,8 +75,9 @@ const displayQuiz = (data) => {
 };
 
 // EventListener for quiz submit button
-document.querySelector("#submit").addEventlistener("click", () => {
-  if (answers.length < 6) {
+document.querySelector("#submit").addEventListener("click", () => {
+  if (answers.length > 6) {
+    // console.log(answers);
     return;
   }
   quizTimer(true);
